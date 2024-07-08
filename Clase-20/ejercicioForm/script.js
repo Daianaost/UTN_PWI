2fa589f38bdc1e3816c27d64acb4f48224593792
dai.ost <doostrovsky@gmail.com>
2024-07-08 09:41:05 -0300

    clases

--- Clase-20/ejercicioForm/script.js
+++ Clase-20/ejercicioForm/script.js
@@ -8,12 +8,3 @@ Cuando se envie el formulario se debera capturar titulo y body y se envia via fe
 Y van a mostrar por consola un 'Enviado'
 */
 
-const loginHTML = document.getElementById('login')
-const handleFormSubmit = (evento) => {
-    evento.preventDefault();
-    console.log('Formulario enviado');
-    console.dir(loginHTML.title.name);
-    console.dir(loginHTML.body.value);
-};
-
-loginHTML.addEventListener('submit', handleFormSubmit);
